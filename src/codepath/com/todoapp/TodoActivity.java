752cc729c733c5eff7fd5062cf298e6644dcd8ea
Prikshit <prikshit@horafin.com>
2019-08-08 12:05:37 +0530

    Initial commit

---
+++ src/codepath/com/todoapp/TodoActivity.java
@@ -0,0 +1,94 @@
+package codepath.com.todoapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+// Figure out why the TodoActivity cannot save the items.
+public class TodoActivity extends Activity {
+
+    ArrayAdapter<String> arrayAdapter;
+    ListView lvItems;
+    ArrayList<String> items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        readItems();
+        setContentView(R.layout.activity_todo);
+        lvItems = (ListView) findViewById(R.id.listView);
+        items = new ArrayList<String>();
+        items.add("Item 1");
+        items.add("Item 2");
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                items.remove(position);
+                arrayAdapter.notifyDataSetChanged();
+                saveItems();
+                return true;
+            }
+        });
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.todo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void addButton(View view) {
+        EditText etItemToAdd = (EditText) findViewById(R.id.etItem);
+        arrayAdapter.add(etItemToAdd.getText().toString());
+        etItemToAdd.setText("");
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+            e.printStackTrace();
+        }
+    }
+
+    private void saveItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
